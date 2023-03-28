33ea88f4f51b16c476863a8af3246c454a87c511
victorgmachado <victorgomesmachado@outlook.com>
2023-03-28 19:04:17 -0300

    ex 1 e 2 feitos

---
+++ fundamentos/secao-03-introducao-a-javaScript/dia-01-javaScript-primeiros-passos/ex02.js
@@ -0,0 +1,9 @@
+let valor1 = 55;
+let valor2 = 104;
+
+if (valor1 > valor2) {
+    console.log("O ", valor1, "é maior que o ", valor2, "!")
+}
+else {
+    console.log("O", valor2, "é maior que o ", valor1, "!")
+}
