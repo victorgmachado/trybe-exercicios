6fdec8ced19969991d4014e374c36adc1d501755
victorgmachado <victorgomesmachado@outlook.com>
2023-03-28 19:35:02 -0300

    ex05 feito

---
+++ fundamentos/secao-03-introducao-a-javaScript/dia-01-javaScript-primeiros-passos/ex05.js
@@ -0,0 +1,10 @@
+let valor1 = 6;
+let valor2 = 6;
+let valor3 = 11;
+
+if (valor1 + valor2 > valor3 && valor2 + valor3 > valor1) {
+    console.log("Com a soma de ", valor1,", ", valor2, " e ", valor3, "é possível formar um triângulo!")
+}
+else {
+    console.log("Esses 3 valores não formam 1 triângulo!")
+}
