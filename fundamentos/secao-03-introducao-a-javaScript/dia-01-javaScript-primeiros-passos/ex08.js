4eca4f6bf29e3d5480e544966ee2a0b8e8130f7c
victorgmachado <victorgomesmachado@outlook.com>
2023-03-29 18:16:47 -0300

    ex08 feito

---
+++ fundamentos/secao-03-introducao-a-javaScript/dia-01-javaScript-primeiros-passos/ex08.js
@@ -0,0 +1,11 @@
+let num1 = 13
+let num2 = 15
+let num3 = 11
+
+if ( num1 % 2 != 0 && num2 % 2 != 0 && num3 % 2 != 0) {
+    console.log('Todos são impares')
+}
+
+else {
+    console.log('Existe um ou mais números pares')
+}
