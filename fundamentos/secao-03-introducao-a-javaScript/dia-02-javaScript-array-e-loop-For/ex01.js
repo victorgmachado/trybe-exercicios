6a4dca5a22f36cc623e3377c8225b416ff285c37
victorgmachado <victorgomesmachado@outlook.com>
2023-03-29 22:45:31 -0300

    ex01

--- fundamentos/secao-03-introducao-a-javaScript/dia-02-javaScript-array-e-loop-For/ex01.js
+++ fundamentos/secao-03-introducao-a-javaScript/dia-02-javaScript-array-e-loop-For/ex01.js
@@ -1,15 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0
 
-for (let i = 0 ; i <= numbers.length ; i += 1 ){
-    soma = soma + numbers[i];
-        if (i >= 9) {
-            console.log(soma);
-        }   
+
+for (let i = 0 ; i <= numbers.length ; i = i + 1 ) {
+    if ( i < numbers.length){
+
+        console.log(numbers[i])
+    }
 }
-
-
     
+
+     
 
 
 
