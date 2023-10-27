faafd25c10f68672952a343d1205a3af64ab5563
victorgmachado <victorgomesmachado@outlook.com>
2023-10-27 20:00:25 -0300

    atividades docker

---
+++ front-end/introducao-react/dia-02/Greeting.tsx
@@ -0,0 +1,29 @@
+// type GreetingProps = {
+//   person: {
+//     firstName: string;
+//     lastName: string;
+//   };
+// };
+
+// function Greeting({ person }: GreetingProps) {
+//   const firstName = person.firstName;
+//   const lastName = person.lastName;
+
+//   return (
+//     <h1 className="greeting">
+//       Olá {`${firstName} ${lastName}`}
+//       <img src="https://i.imgur.com/rqvLd3q.png" alt="" />
+//     </h1>
+//   );
+// }
+
+// const name = {
+//   person: {
+//     firstName: 'Victor',
+//     lastName: 'Gomes',
+//   },
+// };
+
+// console.log(Greeting(name));
+
+// export default Greeting;
